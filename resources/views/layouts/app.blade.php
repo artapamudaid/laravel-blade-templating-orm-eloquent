993addb5fdf2db85aab58bc6a5246f24b6d6e863
pamudaid <ar.pamuda@gmail.com>
2021-12-04 17:10:48 +0700

    landing page blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes.meta')
+    <title>@yield('title') | Landing Page</title>
+
+    <!-- favicon -->
+
+    <link rel="apple-touch-icon" href="">
+    <link rel="shortcut icon" type="image/x-icon" href="">
+
+    <!-- Fungsi stack untuk menyisipkan style atau script untuk file tertentu -->
+    @stack('before-style')
+
+    @include('includes.style')
+
+    @stack('after-style')
+</head>
+
+<body>
+    @include('includes.header')
+
+    @yield('content')
+
+    @include('includes.footer')
+
+    @stack('before-script')
+
+    @include('includes.script')
+
+    @stack('after-script')
+</body>
+
+</html>
