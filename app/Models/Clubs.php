92ff5bf63ddce78ef16bb9ca0c73dd4360a38175
pamudaid <ar.pamuda@gmail.com>
2021-12-06 12:40:26 +0700

    update blade template football analytics

---
+++ app/Models/Clubs.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clubs extends Model
+{
+    // use HasFactory;
+
+    public $table = 'clubs';
+
+    protected $fillable = [
+        'stadiums_id',
+        'name',
+        'logo',
+        'url',
+        'thropy',
+        'created_at',
+        'updated_at'
+    ];
+
+    //relation with stadiums table
+    public function stadiums()
+    {
+        return $this->belongsTo('App\Moddels\Stadiums', 'stadiums_id', 'id');
+    }
+
+    //relation to managers table
+    public function managers()
+    {
+        return $this->hasMany('App\Models\Managers', 'clubs_id');
+    }
+
+    //relation to players table
+    public function players()
+    {
+        return $this->hasMany('App\Models\Players', 'clubs_id');
+    }
+
+    //relation to matches table
+    public function match_clubs()
+    {
+        return $this->hasMany('App\Models\Matches', 'clubs_id');
+    }
+
+    public function match_rivals()
+    {
+        return $this->hasMany('App\Models\Matches', 'rivals_id');
+    }
+}
